b6021c93e3a2717a04ed0fc5af85eb863c0d0b4c
Nikita1814 <kravchenko1814@gmail.com>
2021-09-25 00:45:15 +0300

    feat : add non-swipeable slider to elcome section

--- museum/script.js
+++ museum/script.js
@@ -1,29 +1,48 @@
 const leftBtn = document.querySelector(".slider-button-left")
 const rightBtn = document.querySelector(".slider-button-right")
 const viewWindow = document.querySelector(".welcome-slider-viewer")
-const slider = document.querySelector('.welcome-slider')
-const sliderLen = document.querySelectorAll(".welcome-slider-image")
+const slider = document.querySelector(".welcome-slider")
+const sliderLen = slider.querySelectorAll(".welcome-slider-image").length
 const viewWidth = viewWindow.clientWidth
 
+
 let activeSlide = 0
-slider.syle.left = '1000px'
+slider.style.left = "-1000px"
 
 leftBtn.addEventListener('click', () => {
 switchSlides('left')
 })
+
 rightBtn.addEventListener('click', () => {
     switchSlides('right')
     })
+
+slider.addEventListener('transitionend', CheckI)
+
+
+
 function switchSlides(direction){
 slider.classList.add('visual-transition')
 
 if (direction === "left"){
-    activeSlide --    
+    activeSlide ++
 }
 
 if (direction === "right"){
-    activeSlide ++
+    activeSlide --
 }
 slider.style.transform = `translateX(${activeSlide * viewWidth}px)`
 }
 
+
+function  CheckI(){
+slider.classList.remove('visual-transition')
+if(activeSlide > 0 ){
+    activeSlide = -4
+    slider.style.transform = `translateX(${activeSlide * viewWidth}px)`
+}
+if(activeSlide < -4 ){
+activeSlide = 0
+slider.style.transform = `translateX(${activeSlide * viewWidth}px)`
+}
+}
