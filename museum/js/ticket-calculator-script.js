19b6de9bd13cf9f22e100fe57068bc2de5533f3d
Nikita1814 <kravchenko1814@gmail.com>
2021-10-08 03:15:28 +0300

    feat: start adding  ticket form calculator

--- museum/js/ticket-calculator-script.js
+++ museum/js/ticket-calculator-script.js
@@ -1,9 +1,20 @@
+//section-vars
 const ticketBtns = document.querySelectorAll('.number-btn')
 const sectionTotal = document.querySelector('.total-in-section')
 const types = document.querySelectorAll('.section-ticket-check')
 const senTicketCount = document.querySelector('.senior-tickets')
 const baseTicketCount = document.querySelector('.basic-tickets')
- 
+
+//Form-vars
+const formTicketBtns = document.querySelectorAll('.number-btn-form')
+const typeOptions = document.getElementById('TicketType')
+const formBase = document.querySelectorAll('.form-basic')
+const formSen = document.querySelectorAll('.form-senior')
+const formBaseTotal = document.querySelector('.form-total-basic')
+const formSenTotal = document.querySelector('.form-total-senior')
+const formTotal = document.querySelector('.form-total-cost')
+
+
 const priceList = {
 permanent: 20,
 temporary: 25,
@@ -12,11 +23,17 @@ combined: 40,
 
 
 ticketBtns.forEach((el) => el.addEventListener('click', () =>{totalUpd()}))
+formTicketBtns.forEach((el) => el.addEventListener('click', () =>{totalUpd()}))
 
-function totalUpd(){
+function totalUpd(){    
 let ticketType
 types.forEach((el) => {if (el.checked) {ticketType = el.value}})
-sectionTotal.innerText = `Total €${priceList[ticketType] * baseTicketCount.value + priceList[ticketType] * (senTicketCount.value/2)}`
+formBase.forEach((el) => el.innerText = `Basic (${priceList[ticketType]} €)`)
+formSen.forEach((el) => el.innerText = `Senior (${priceList[ticketType]/2} €)`)
+formBaseTotal.innerText = `${priceList[ticketType] * baseTicketCount.value} €`
+formSenTotal.innerText = `${priceList[ticketType]/2 * senTicketCount.value} €`
+formTotal.innerText = ` ${priceList[ticketType] * baseTicketCount.value + priceList[ticketType]/2 * senTicketCount.value} €`
+sectionTotal.innerText = `Total ${priceList[ticketType] * baseTicketCount.value + priceList[ticketType]/2 * senTicketCount.value}€`
 }
 
 
