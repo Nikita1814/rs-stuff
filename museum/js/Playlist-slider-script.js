11fff15fc8c104809b4772c8b923cbea195312df
Nikita1814 <kravchenko1814@gmail.com>
2021-10-12 01:05:47 +0300

    feat : make playlist slider slide

--- museum/js/Playlist-slider-script.js
+++ museum/js/Playlist-slider-script.js
@@ -2,7 +2,7 @@ const vidLeftBtn = document.querySelector(".video-slider-left");
 const vidRightBtn = document.querySelector(".video-slider-right");
 const vidViewWindow = document.querySelector(".video-slider");
 const vidSlider = document.querySelector('.video-sliding');
-const vidSliderLen = document.querySelectorAll(".video-placeholder").length;
+const vidSliderLen = document.querySelectorAll(".video-placeholder").length
 const vidSlide = document.querySelector('.video-placeholder')
 const vidMarkerList = document.querySelectorAll(".video-slider-marker");
 const vidViewWidth = vidViewWindow.clientWidth;
@@ -10,7 +10,6 @@ const vidViewWidth = vidViewWindow.clientWidth;
 let vidActiveMarker = vidMarkerList[0]
 vidActiveMarker.style.backgroundColor = '#333333'
 let vidActiveSlide = 0
-vidSlider.style.left ='0'
 vidLeftBtn.addEventListener('click', () => {
     vidSwitchSlides('left')
     })
@@ -26,14 +25,18 @@ vidRightBtn.addEventListener('click', () => {
            
             if (direction === "left"){
                 vidActiveSlide ++
-                
+                if(vidActiveSlide > 0 ) {
+                    vidActiveSlide = -2
+                }
             }
             
             if (direction === "right"){
                vidActiveSlide --
+              if (vidActiveSlide <  -2) {
+                vidActiveSlide = 0
+            }
             }
 
-            vidSlider.style.transform = `translateX(${vidActiveSlide * vidSlide.offsetWidth}px)`
+            vidSlider.style.transform = `translateX(${vidActiveSlide * 497 }px)`
             vidActiveMarker.style.backgroundColor = '#333333'
         }
-        
