d2e8626e987067badcbf83d7d65b802da630fe05
Nikita1814 <kravchenko1814@gmail.com>
2021-10-23 20:47:10 +0300

    feat: bg image change from collection

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,140 +1,192 @@
-
 //lang var
-let lang = "en-US"
-
+let lang = "en-US";
 
 //date vars
 
-const clock = document.querySelector('.time')
-const dateDiv = document.querySelector('.date')
-const dateOptions = {weekday:'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second:'numeric', timeZone: 'Europe/Minsk', hour12: false,};
-const greetSpan = document.querySelector('.greeting')
+const clock = document.querySelector(".time");
+const dateDiv = document.querySelector(".date");
+const dateOptions = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  timeZone: "Europe/Minsk",
+  hour12: false,
+};
+const greetSpan = document.querySelector(".greeting");
 
 //quotes vars
 
-const nom = document.querySelector('.name')
-const quoteDiv = document.querySelector('.quote')
-const authorDiv = document.querySelector('.author')
-const changeQuote = document.querySelector('.change-quote')
+const nom = document.querySelector(".name");
+const quoteDiv = document.querySelector(".quote");
+const authorDiv = document.querySelector(".author");
+const changeQuote = document.querySelector(".change-quote");
 
 //weather widget vars
-let cityInput = document.querySelector('.city')
-const weatherIcon = document.querySelector('.weather-icon')
-const temperature = document.querySelector('.temperature')
-const weatherDesc = document.querySelector('.weather-description')
-const wind = document.querySelector('.wind')
-const humidity = document.querySelector('.humidity')
-const weatherError = document.querySelector('.weather-error')
+let cityInput = document.querySelector(".city");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const weatherDesc = document.querySelector(".weather-description");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+const weatherError = document.querySelector(".weather-error");
 
-// time setting function 
-function loadTime (){
-let date = new Date
-let dateStr = date.toLocaleString(`${lang}`, dateOptions)
-let dateSplitArr = dateStr.split(',')
-clock.textContent = `${dateSplitArr[2]}`
-dateDiv.textContent = `${dateSplitArr[0]}, ${dateSplitArr[1]} `
+//Background image sliding vars
+const leftBtn = document.querySelector(".slide-prev");
+const rightBtn = document.querySelector(".slide-next");
+let collection = "evening";
+let bgImgNum = RandNumGen();
 
-setTimeout(loadTime, 1000)
+// time setting function
+function loadTime() {
+  let date = new Date();
+  let dateStr = date.toLocaleString(`${lang}`, dateOptions);
+  let dateSplitArr = dateStr.split(",");
+  clock.textContent = `${dateSplitArr[2]}`;
+  dateDiv.textContent = `${dateSplitArr[0]}, ${dateSplitArr[1]} `;
+
+  setTimeout(loadTime, 1000);
 }
-loadTime()
-
+loadTime();
 
 // greeting setting function
 
-function loadGreeting(){
-    let date = new Date
-    let hr = date.getHours()
-    let greeting = 'Good'
-   if(hr >=6 && hr< 12){
-    greeting = 'Good morning,'
-   } else if( hr >=12 && hr < 18){
-    greeting = 'Good afternoon,' 
-   }  else if( hr >=18 && hr <=23){
-    greeting = 'Good evening,' 
-   }  else if( hr >=0 && hr < 6){
-    greeting = 'Good night,' 
-   }
- greetSpan.textContent = greeting
+function loadGreeting() {
+  let date = new Date();
+  let hr = date.getHours();
+  let greeting = "Good";
+  if (hr >= 6 && hr < 12) {
+    collection = "morning";
+    greeting = "Good morning,";
+  } else if (hr >= 12 && hr < 18) {
+    collection = "afternoon";
+    greeting = "Good afternoon,";
+  } else if (hr >= 18 && hr <= 23) {
+    collection = "evening";
+    greeting = "Good evening,";
+  } else if (hr >= 0 && hr < 6) {
+    collection = "night";
+    greeting = "Good night,";
+  }
+  greetSpan.textContent = greeting;
 }
-loadGreeting()
+loadGreeting();
 
 // functions and listeners for  saving the name to local storage and loading it
 
-function setStorage () {
-    localStorage.setItem('name', nom.value); 
+function setStorage() {
+  localStorage.setItem("name", nom.value);
 }
 
-function loadStorage (){
-    if(localStorage.getItem('name')) {
-        nom.value = localStorage.getItem('name');
-      }
-
+function loadStorage() {
+  if (localStorage.getItem("name")) {
+    nom.value = localStorage.getItem("name");
+  }
 }
-window.addEventListener('beforeunload', setStorage)
-window.addEventListener('load', loadStorage)
+window.addEventListener("beforeunload", setStorage);
+window.addEventListener("load", loadStorage);
 
 // quotes
 
-async function loadQuotes(){
-    const quotes = 'quotes.json'
-    const res = await fetch(quotes)
-    const data = await res.json();
-    let quote = data[Math.round(Math.random() * ((data.length -1) - 0) + 0)]
-    quoteDiv.textContent = quote.text
-    authorDiv.textContent =quote.author 
-    changeQuote.addEventListener('click', () =>{
-        quote = data[Math.round(Math.random() * ((data.length -1) - 0) + 0)]
-    quoteDiv.textContent = quote.text
-    authorDiv.textContent =quote.author 
-    }) 
+async function loadQuotes() {
+  const quotes = "quotes.json";
+  const res = await fetch(quotes);
+  const data = await res.json();
+  let quote = data[Math.round(Math.random() * (data.length - 1 - 0) + 0)];
+  quoteDiv.textContent = quote.text;
+  authorDiv.textContent = quote.author;
+  changeQuote.addEventListener("click", () => {
+    quote = data[Math.round(Math.random() * (data.length - 1 - 0) + 0)];
+    quoteDiv.textContent = quote.text;
+    authorDiv.textContent = quote.author;
+  });
 }
-loadQuotes()
+loadQuotes();
 
 //weather widget
 
-async function loadWeather(){
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&lang=${lang[0]}${lang[1]}&appid=14ea945df3b3517b12a5d7dfc6a3f40b&units=metric`
-let res = await fetch(url)
-if(!res.ok){
- weatherError.textContent = `Error! city not found for "${cityInput.value}"`   
- weatherIcon.classList.remove(`owf`)
- temperature.textContent = ``
- weatherDesc.textContent = ``
- wind.textContent =``
- humidity.textContent =``
-} else {
-weatherError.textContent = ``   
-let data = await res.json()
-console.log(url)
-console.log(data.weather[0].id, data.weather[0].description, data.main.temp, data.main.humidity, data.wind.speed);
-if (lang === 'en-US'){
-
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`)
-    weatherIcon.classList.add(`owf`)
-    temperature.textContent = `${data.main.temp}°C`
-    weatherDesc.textContent = data.weather[0].description
-    wind.textContent =`Wind speed: ${data.wind.speed} m/s `
-    humidity.textContent =`Humidity: ${data.main.humidity}`
-    
-    
-    
-} else if (lang === 'ru-RU'){
-
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`)
-    weatherIcon.classList.add(`owf`)
-    temperature.textContent = `${data.main.temp}°C`
-    weatherDesc.textContent = data.weather[0].description
-    wind.textContent =`Скорость ветра: ${data.wind.speed} м/с `
-    humidity.textContent =`Влажность: ${data.main.humidity}` 
-    
+async function loadWeather() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&lang=${lang[0]}${lang[1]}&appid=14ea945df3b3517b12a5d7dfc6a3f40b&units=metric`;
+  let res = await fetch(url);
+  if (!res.ok) {
+    weatherError.textContent = `Error! city not found for "${cityInput.value}"`;
+    weatherIcon.classList.remove(`owf`);
+    temperature.textContent = ``;
+    weatherDesc.textContent = ``;
+    wind.textContent = ``;
+    humidity.textContent = ``;
+  } else {
+    weatherError.textContent = ``;
+    let data = await res.json();
+    console.log(url);
+    console.log(
+      data.weather[0].id,
+      data.weather[0].description,
+      data.main.temp,
+      data.main.humidity,
+      data.wind.speed
+    );
+    if (lang === "en-US") {
+      weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+      weatherIcon.classList.add(`owf`);
+      temperature.textContent = `${data.main.temp}°C`;
+      weatherDesc.textContent = data.weather[0].description;
+      wind.textContent = `Wind speed: ${data.wind.speed} m/s `;
+      humidity.textContent = `Humidity: ${data.main.humidity}`;
+    } else if (lang === "ru-RU") {
+      weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+      weatherIcon.classList.add(`owf`);
+      temperature.textContent = `${data.main.temp}°C`;
+      weatherDesc.textContent = data.weather[0].description;
+      wind.textContent = `Скорость ветра: ${data.wind.speed} м/с `;
+      humidity.textContent = `Влажность: ${data.main.humidity}`;
+    }
+  }
 }
+
+loadWeather();
+
+cityInput.addEventListener("input", loadWeather);
+
+// bg image slider
+function RandNumGen() {
+  let randNum = `${Math.round(Math.random() * (20 - 1 - 1) + 1)}`.padStart(
+    2,
+    "0"
+  );
+  return randNum;
 }
+
+function setBg() {
+  document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/Nikita1814/stage1-tasks/assets/images/${collection}/${bgImgNum}.jpg')`;
 }
+setBg();
 
- loadWeather()
+function slideBg(direction) {
+  if (direction === "left") {
+    bgImgNum = `${Number(bgImgNum) - 1}`.padStart(2, "0");
+    if (Number(bgImgNum) === 0) {
+      bgImgNum = "20";
+    }
+  }
+  if (direction === "right") {
+    bgImgNum = `${Number(bgImgNum) + 1}`.padStart(2, "0");
+    if (Number(bgImgNum) === 21) {
+      bgImgNum = "01";
+    }
+  }
+  document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/Nikita1814/stage1-tasks/assets/images/${collection}/${bgImgNum}.jpg')`;
+}
+leftBtn.addEventListener('click', ()=>{
+    slideBg('left') 
+})
+rightBtn.addEventListener('click', ()=>{
+    slideBg('right') 
+})
 
-cityInput.addEventListener('input', loadWeather)
-
+console.log(Number("01"));
 
 /*console.log(`${lang[0]}${lang[1]}`)
 console.log(`${cityInput.value}`)*/
