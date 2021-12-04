0af53d9d2fc6dc26155917638e57c92fc79bb777
Nikita1814 <kravchenko1814@gmail.com>
2021-12-04 21:49:09 +0300

    init: copy app files and start work on task

---
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: 'e00c260ec6ca404a907d3198e17cdf10', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
