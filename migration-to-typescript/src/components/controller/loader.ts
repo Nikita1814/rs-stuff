aa513e65e504f8c69809ec5d452f68d76c3abbbb
Nikita1814 <kravchenko1814@gmail.com>
2021-12-05 00:43:47 +0300

    feat: turned all js files to ts and add NewsApi object interfaces

--- migration-to-typescript/src/components/controller/loader.ts
+++ migration-to-typescript/src/components/controller/loader.ts
@@ -1,3 +1,39 @@
+interface Article{
+    source:{
+        id: string,
+        name: string
+        },
+        author: string,
+        title: string,
+        description: string,
+        url: string,
+        urlToImage: string,
+        publishedAt: string,
+        content: string
+}
+
+interface Source{
+id: string,
+name: string,
+description: string,
+url: string,
+category: string,
+language: string,
+country: string
+}
+
+
+interface ArtResp{
+    status: string,
+    totalResults: number,
+    articles: Article[]
+}
+
+interface SourceResp{
+    status:string,
+    sources: Source[]
+}
+
 class Loader {
     constructor(baseLink, options) {
         this.baseLink = baseLink;
