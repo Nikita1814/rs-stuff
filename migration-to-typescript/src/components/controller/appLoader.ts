1aa35e20426214e995f52399b7c5ab5829794200
Nikita1814 <kravchenko1814@gmail.com>
2021-12-05 22:07:34 +0300

    feat: change url in appLoader.Js to  https://nodenews.herokuapp.com/ so that it would work when deployed

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'e00c260ec6ca404a907d3198e17cdf10', // получите свой ключ https://newsapi.org/
         });
     }
