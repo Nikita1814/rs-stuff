0252ea6a767a5e2fc09461fe7fe23aae684d75b8
Nikita1814 <kravchenko1814@gmail.com>
2021-12-09 14:52:09 +0300

    refactor: format css files

--- migration-to-typescript/src/components/view/appView.ts
+++ migration-to-typescript/src/components/view/appView.ts
@@ -1,24 +1,24 @@
-import { ArtResp, SourceResp } from '../controller/loader';
-import News from './news/news';
-import Sources from './sources/sources';
+import { ArtResp, SourceResp } from "../controller/loader";
+import News from "./news/news";
+import Sources from "./sources/sources";
 
 export class AppView {
-    news
-    sources
-    constructor() {
-        this.news = new News();
-        this.sources = new Sources();
-    }
+  news;
+  sources;
+  constructor() {
+    this.news = new News();
+    this.sources = new Sources();
+  }
 
-    drawNews(data: ArtResp) {
-        const values = data?.articles ? data?.articles : [];
-        this.news.draw(values);
-    }
+  drawNews(data: ArtResp) {
+    const values = data?.articles ? data?.articles : [];
+    this.news.draw(values);
+  }
 
-    drawSources(data: SourceResp) {
-        const values = data?.sources ? data?.sources : [];
-        this.sources.draw(values);
-    }
+  drawSources(data: SourceResp) {
+    const values = data?.sources ? data?.sources : [];
+    this.sources.draw(values);
+  }
 }
 
 export default AppView;
