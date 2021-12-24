d3e79678c728f961ee707319f93f94ad6de17da3
Nikita1814 <kravchenko1814@gmail.com>
2021-12-24 15:25:51 +0300

    fix: remove unnecessary my-app folder

--- my-app/src/index.ts
+++
@@ -1,6 +0,0 @@
-import App from './components/app/app'
-import { AppСlass } from './components/interfaces/interfaces'
-
-/*TODO connect the app class here and initialize the app*/
-const app: AppСlass = new App()
-app.start()
