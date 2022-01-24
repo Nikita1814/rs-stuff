529826e1bba9303486277de2bcd177e80872c6e6
Nikita1814 <kravchenko1814@gmail.com>
2022-01-24 23:48:54 +0300

    refactor: add self evaluation

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,7 +1,13 @@
 import App from './components/app/app'
 console.log(`Cамооценка \n
-200/200\n 
-доп ф-нала нет\n
-`)
+180+/190\n 
+не выполнил пункт сохранения стейта страниц в локал сторадж\n
+доп ф-нал: добавил таймер (сообщение с результатом гонки появляется после него), баллы на ваше усмотрение\n
+\n
+Basic structure - 15\n
+"Garage" view - 50\n
+Car animation - 50\n
+Race animation - 30\n
+"Winners" view - 35\n`)
 const app = new App()
 app.run()
